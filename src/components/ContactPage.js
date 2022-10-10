0f9a567e11964cd48c064b6cb592be0c11fbae4d
marcusloy77 <marcus.loy.77@gmail.com>
2022-10-10 17:31:52 +1100

    all done yaygit add .git add .

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -11,24 +11,25 @@ const ContactPage = () => {
   }
 
   const longStringProcessor = (str) => {
-    if (str.length > 30) {
-      return str.slice(0,30) + '...'
-    }
+    return str
+    // if (str.length > 30) {
+    //   return str.slice(0,30) + '...'
+    // }
   }
   
   return (
     <div className="contactPage">
       <div className='title'><p>{contact.name}</p></div>
       <div className='actionBtnsContact'>
-        <Link className='boxBtn' to={'/' + contact.username + '/message'}><div className='box'><div className='boxIcon'><AiFillMessage size='1.5em'/></div><p className='boxWord'>Msg</p></div></Link>
+        <Link className='boxBtn' to={'/' + contact.username + '/message'}><div className='box'><div className='boxIcon'><AiFillMessage size='1.5em'/></div><p className='iconWord'>Msg</p></div></Link>
         <Link className='boxBtn' to={'/call?number=' + contact.phone}><div className='box'><div className='boxIcon'><FaPhoneAlt size='1.5em'/></div><p className='iconWord'>Call </p></div></Link>
         <Link className='boxBtn' to='/webcam'><div className='box'><div className='boxIcon'><FaVideo size='1.5em'/></div><p className='iconWord'>Vid</p></div></Link>
         <a className='boxBtn' href={'mailto:' + contact.email}><div className='box'><div className='boxIcon'><AiFillMail size='1.5em'/></div><p className='iconWord'>Mail</p></div></a></div>
         <Link className='contactOptions' to={'/call?number=' + contact.phone}><div>Phone: {contact.phone}</div></Link>
-      <Link className='contactOptions' to={'mailto:' + contact.email}><div >Email: {contact.email}</div></Link>
-      <Link className='contactOptions' to={'https://www.google.com/search?q=' + contact.company.name.replace(' ', '+')}><div >Company: {contact.company.name}</div></Link>
-      <Link className='contactOptions' to={'http://' + contact.website}><div >Website: {contact.website}</div></Link>
-      <Link className='contactOptions' to={placeLink()}><div>{longStringProcessor(contact.address.suite + ' ' + contact.address.street + ' ' + contact.address.city + ' ' + contact.address.zipcode)}</div></Link>
+      <a className='contactOptions' href={'mailto:' + contact.email}><div >Email: {contact.email}</div></a>
+      <a className='contactOptions' href={'https://www.google.com/search?q=' + contact.company.name.replace(' ', '+')}><div >Company: {contact.company.name}</div></a>
+      <a className='contactOptions' href={'http://' + contact.website}><div >Website: {contact.website}</div></a>
+      <a className='contactOptions' href={placeLink()}><div>{'Lives at: ' + contact.address.suite + ' ' + contact.address.street + ' ' + contact.address.city + ' ' + contact.address.zipcode}</div></a>
 
     </div>
   )
